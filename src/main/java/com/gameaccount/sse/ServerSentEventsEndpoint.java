5c107e0567ce462ea85712530d332e188647267e
Edinardo Potrich <epotrich@gameaccount.com>
2020-10-01 23:34:21 -0700

    Added keepalive message

--- src/main/java/com/gameaccount/sse/ServerSentEventsEndpoint.java
+++ src/main/java/com/gameaccount/sse/ServerSentEventsEndpoint.java
@@ -3,10 +3,9 @@ package com.gameaccount.sse;
 import java.time.Duration;
 import java.time.LocalTime;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +17,19 @@ import reactor.core.publisher.Flux;
 @RequestMapping ( "/event" )
 public class ServerSentEventsEndpoint {
 
-    private Logger log = LoggerFactory.getLogger(ServerSentEventsEndpoint.class);
     @Value("${message.ofMinutes}")
     private Long minutes;
 
     @CrossOrigin
     @GetMapping(path = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<LocalTime> sse() {
-        return Flux
+    public Flux<ServerSentEvent<LocalTime>> sse() {
+        Flux<ServerSentEvent<LocalTime>> data = Flux
                 .interval(Duration.ofMinutes(minutes))
-                .log()
-                .map(sequence -> LocalTime.now());
+                .map(sequence -> ServerSentEvent.<LocalTime>builder().data(LocalTime.now()).build());
+        Flux<ServerSentEvent<LocalTime>> keepalive = Flux
+                .interval(Duration.ofSeconds(15))
+                .map(sequence -> ServerSentEvent.<LocalTime>builder().comment("keepalive").build());
+        return Flux.merge(data, keepalive).log();
     }
-
+    
 }
